84fb1139ea399dc144f1d8ea3863032119a35735
Soheil <soheilhesar@gmail.com>
2025-09-13 16:28:02 +0330

    add repository for Place order

---
+++ src/data/repo/place_order_repo.py
@@ -0,0 +1,34 @@
+from sqlalchemy import select,func
+from sqlalchemy.orm import Session
+
+from src.data.core.models import PlaceOrder
+
+
+class PlaceOrderRepo:
+
+
+
+    def __init__(self, session : Session):
+        self.session = session
+
+
+
+    def create(self, place_order : PlaceOrder):
+        pass
+
+
+    def generate_place_order_code(self) -> str | None:
+
+        result = self.session.execute(
+            select(func.max(PlaceOrder.order_code))
+        ).scalar_one_or_none()
+
+        if result is None:
+            start_code = 10000
+            return str(start_code)
+
+        try:
+            generated_code = int(result) + 1
+            return str(generated_code)
+        except Exception:
+            return None
