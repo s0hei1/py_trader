58f0aa5bbf4adb8684ff29930f805e1d1ed9bde6
Soheil <soheilhesar@gmail.com>
2025-09-30 22:09:53 +0330

    implement strategies

--- third_party/auto_trader/simple_ma_strategy.py
+++ third_party/auto_trader/simple_ma_strategy.py
@@ -10,24 +10,71 @@ from third_party.mt5_overhead.ordertype import OrderType, OrderTypes
 
 class StrategyProtocol:
 
-    def init_history(self,*args, **kwargs):...
+    def init_history(self, *args, **kwargs): ...
 
-    def check_signal(self,data): ...
+    def check_signal(self, data): ...
+
 
 class SimpleMAStrategy:
+    candles_df: pd.DataFrame | None = None
 
-    candles_df : pd.DataFrame | None = None
-
-    async def setup(self, data : Chart):
+    async def setup(self, data: Chart):
         if self.candles_df is None:
-            self.candles_df = data.to_dataframe()
-        # else:
-        #     await self.next(data.to_dataframe())
+            self.candles_df = await self.trasnform(data.to_dataframe())
+        else:
+            self.candles_df = pd.concat([
+                self.candles_df,
+                data.to_dataframe()
+            ])
+            self.candles_df = await self.trasnform(self.candles_df)
+            self.candles_df = await self.clean_memory(self.candles_df)
+            await self.next(self.candles_df)
 
-    async def next(self, data : pd.DataFrame):
-        self.candles_df = pd.concat(
-            [self.candles_df,
-            data]
+    async def clean_memory(self, df: pd.DataFrame):
+        if len(df) < 500:
+            return
+
+        df = df.iloc[-500:-1]
+        return df
+
+    async def trasnform(self, df: pd.DataFrame):
+
+        df['atr'] = talib.ATR(
+            high=df['high'],
+            low=df['low'],
+            close=df['close'],
+            timeperiod=21
         )
-        print(self.candles_df)
+        df['ma'] = talib.MA(
+            real=df['close'],
+            timeperiod=21
+        )
 
+        def set_ma_status(row):
+            if row['close'] > row['ma']:
+                return 'upper'
+            if row['close'] < row['ma']:
+                return 'under'
+            return 'eq'
+
+        df['ma_status'] = df.apply(set_ma_status, axis=1)
+
+        return df
+
+    async def next(self, data: pd.DataFrame):
+
+        last_ma_status = data.iloc[-1]['ma_status']
+        pre_last_ma_status = data.iloc[-2]['ma_status']
+
+        if last_ma_status == pre_last_ma_status:
+            return None
+
+
+
+
+        return TradeSignal(
+            order_type: ,
+            entry_price:,
+            stop_loss_price:,
+            take_profit_price:,
+        )
